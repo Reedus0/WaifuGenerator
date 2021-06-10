68809b37b6c743e2d084b7f02160b2c883d7464e
Reedus0 <lazsasha2006@gmail.com>
2021-06-11 01:11:51 +0300

    Added some new styles, added repeat function after making an image, fixed null letter bug

--- WaifuGenerator/WaifuGenerator/Program.cs
+++ WaifuGenerator/WaifuGenerator/Program.cs
@@ -1,86 +1,14 @@
 ﻿using System;
-
-
 namespace WaifuGenerator
 {
     class Program
     {
-        public static string UserName { get; }
-        static void Main(string[] args)
+        public static string link;
+
+        public static void Download(string Link)
         {
-            System.Net.WebRequest reqGET = null;
-            Console.WriteLine("SFW or NSFW?");
-            string nsfwStr = Console.ReadLine();
-            Console.WriteLine("Types:\n");
-            bool nsfw = false;
-            string type = "";
-            if (nsfwStr.StartsWith("n"))
-            {
-                nsfw = true;
-                Console.WriteLine("waifu\n");
-                Console.WriteLine("neko\n");
-                Console.WriteLine("blowjob\n");
-                type = Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("waifu\n");
-                Console.WriteLine("neko\n");
-                Console.WriteLine("kiss\n");
-                Console.WriteLine("dance\n");
-                Console.WriteLine("smile\n");
-                type = Console.ReadLine();
-            }
-            if (nsfw)
-            {
-                string word = "";
-                char letter = type[0];
-                switch(letter.ToString())
-                {
-                    case "w":
-                        word = "waifu";
-                        break;
-                    case "n":
-                        word = "neko";
-                        break;
-                    case "b":
-                        word = "blowjob";
-                        break;
-                    default:
-                        word = "waifu";
-                        break;
-                }
-                string link = String.Format("https://api.waifu.pics/nsfw/{0}", word);
-                reqGET = System.Net.WebRequest.Create(link);
-            }
-            else
-            {
-                string word = "";
-                char letter = type[0];
-                switch (letter.ToString())
-                {
-                    case "w":
-                        word = "waifu";
-                        break;
-                    case "n":
-                        word = "neko";
-                        break;
-                    case "k":
-                        word = "kiss";
-                        break;
-                    case "d":
-                        word = "dance";
-                        break;
-                    case "s":
-                        word = "smile";
-                        break;
-                    default:
-                        word = "waifu";
-                        break;
-                }
-                string link = String.Format("https://api.waifu.pics/sfw/{0}", word);
-                reqGET = System.Net.WebRequest.Create(link);
-            }
+            System.Net.WebRequest reqGET;
+            reqGET = System.Net.WebRequest.Create(Link);
             System.Net.WebResponse resp = reqGET.GetResponse();
             System.IO.Stream stream = resp.GetResponseStream();
             System.IO.StreamReader sr = new System.IO.StreamReader(stream);
@@ -101,12 +29,12 @@ namespace WaifuGenerator
             }
             var result = String.Join("", letters);
             string UserName = Environment.UserName;
-            for(int k = 0; k < 3; k++)
+            for (int k = 0; k < 3; k++)
             {
                 result = result.Remove(result.Length - 1);
             }
             string localFilename;
-            if (result.Substring(result.Length-1) == "f")
+            if (result.Substring(result.Length - 1) == "f")
             {
                 localFilename = "WaifuGif.gif";
                 try
@@ -115,7 +43,7 @@ namespace WaifuGenerator
                 }
                 catch
                 {
-                    
+
                 }
             }
             else
@@ -134,8 +62,151 @@ namespace WaifuGenerator
             {
                 client.DownloadFile(result, localFilename);
             }
+        }
+        public static string Request()
+        {
+            System.Net.WebRequest reqGET = null;
+            Console.WriteLine("SFW or NSFW?");
+            string nsfwStr = Console.ReadLine().ToLower();
+            Console.WriteLine("Types:\n");
+            bool nsfw = false;
+            if (nsfwStr != null)
+            {
+                nsfw = false;
+            }
+            else
+            {
+                nsfw = false;
+            }
+            string type = "";
+            if (nsfwStr.StartsWith("n"))
+            {
+                nsfw = true;
+                Console.WriteLine("waifu\n");
+                Console.WriteLine("neko\n");
+                Console.WriteLine("blowjob\n");
+                type = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("waifu\n");
+                Console.WriteLine("neko\n");
+                Console.WriteLine("kiss\n");
+                Console.WriteLine("dance\n");
+                Console.WriteLine("smile\n");
+                Console.WriteLine("highfive\n");
+                Console.WriteLine("cry\n");
+                type = Console.ReadLine();
+            }
+            if (nsfw)
+            {
+                string word = "";
+                string typeLow;
+                if (type != "")
+                {
+                    typeLow = type.ToLower();
+                }
+                else
+                {
+                    typeLow = "waifu";
+                }
+                char letter = typeLow[0];
+                switch (letter.ToString())
+                {
+                    case "w":
+                        word = "waifu";
+                        break;
+                    case "n":
+                        word = "neko";
+                        break;
+                    case "b":
+                        word = "blowjob";
+                        break;
+                    default:
+                        word = "waifu";
+                        break;
+                }
+                string someLink = String.Format("https://api.waifu.pics/nsfw/{0}", word);
+                return someLink;
+            }
+            else
+            {
+                string word = "";
+                string typeLow;
+                if (type != "")
+                {
+                    typeLow = type.ToLower();
+                }
+                else
+                {
+                    typeLow = "waifu";
+                }
+                char letter = typeLow[0];
+                switch (letter.ToString())
+                {
+                    case "w":
+                        word = "waifu";
+                        break;
+                    case "n":
+                        word = "neko";
+                        break;
+                    case "k":
+                        word = "kiss";
+                        break;
+                    case "d":
+                        word = "dance";
+                        break;
+                    case "s":
+                        word = "smile";
+                        break;
+                    case "р":
+                        word = "highfive";
+                        break;
+                    case "c":
+                        word = "cry";
+                        break;
+                    default:
+                        word = "waifu";
+                        break;
+                }
+                string someLink = String.Format("https://api.waifu.pics/sfw/{0}", word);
+                return someLink;
+            }
+        }
+        public static void Iterator()
+        {
+            char letter2 = 'y';
+            string answer = "";
             Console.WriteLine("Done, check the folder");
-            Console.ReadLine();
+            Console.WriteLine("Make another one? Y/n");
+            answer = Console.ReadLine();
+            string answerLow = answer.ToLower();
+            if(answer != "") {
+                letter2 = answerLow[0];
+            }
+            else
+            {
+                letter2 = 'y';
+            }
+
+            string letterStr = letter2.ToString();
+            if (letterStr == "n")
+            {
+                System.Windows.Forms.Application.Restart();
+            }
+            else
+            {
+                Download(link);
+                Iterator();
+            }
+        }
+        public static string UserName { get; }
+        static void Main(string[] args)
+        {
+            link = Request();
+            Download(link);
+            Iterator();
         }
     }
 }
+
