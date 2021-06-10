11edff96fb5cb5b0613c5d1d00607f4b84a409ed
Reedus0 <lazsasha2006@gmail.com>
2021-06-10 16:36:46 +0300

    Added type selection, added style selection, fixed folder

--- WaifuGenerator/WaifuGenerator/Program.cs
+++ WaifuGenerator/WaifuGenerator/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
-using System.Collections.Generic;
-using System.Security.Principal;
+
 
 namespace WaifuGenerator
 {
@@ -10,12 +8,84 @@ namespace WaifuGenerator
         public static string UserName { get; }
         static void Main(string[] args)
         {
-            System.Net.WebRequest reqGET = System.Net.WebRequest.Create(@"https://api.waifu.pics/sfw/waifu");
+            System.Net.WebRequest reqGET = null;
+            Console.WriteLine("SFW or NSFW?");
+            string nsfwStr = Console.ReadLine();
+            Console.WriteLine("Types:\n");
+            bool nsfw = false;
+            string type = "";
+            if (nsfwStr.StartsWith("n"))
+            {
+                nsfw = true;
+                Console.WriteLine("waifu\n");
+                Console.WriteLine("neko\n");
+                Console.WriteLine("blowjob\n");
+                type = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("waifu\n");
+                Console.WriteLine("neko\n");
+                Console.WriteLine("kiss\n");
+                Console.WriteLine("dance\n");
+                Console.WriteLine("smile\n");
+                type = Console.ReadLine();
+            }
+            if (nsfw)
+            {
+                string word = "";
+                char letter = type[0];
+                switch(letter.ToString())
+                {
+                    case "w":
+                        word = "waifu";
+                        break;
+                    case "n":
+                        word = "neko";
+                        break;
+                    case "b":
+                        word = "blowjob";
+                        break;
+                    default:
+                        word = "waifu";
+                        break;
+                }
+                string link = String.Format("https://api.waifu.pics/nsfw/{0}", word);
+                reqGET = System.Net.WebRequest.Create(link);
+            }
+            else
+            {
+                string word = "";
+                char letter = type[0];
+                switch (letter.ToString())
+                {
+                    case "w":
+                        word = "waifu";
+                        break;
+                    case "n":
+                        word = "neko";
+                        break;
+                    case "k":
+                        word = "kiss";
+                        break;
+                    case "d":
+                        word = "dance";
+                        break;
+                    case "s":
+                        word = "smile";
+                        break;
+                    default:
+                        word = "waifu";
+                        break;
+                }
+                string link = String.Format("https://api.waifu.pics/sfw/{0}", word);
+                reqGET = System.Net.WebRequest.Create(link);
+            }
             System.Net.WebResponse resp = reqGET.GetResponse();
             System.IO.Stream stream = resp.GetResponseStream();
             System.IO.StreamReader sr = new System.IO.StreamReader(stream);
             var s = sr.ReadToEnd();
-            List<string> letters = new List<string>();
+            System.Collections.Generic.List<string> letters = new System.Collections.Generic.List<string>();
             int j = 0;
             foreach (char i in s)
             {
@@ -35,13 +105,37 @@ namespace WaifuGenerator
             {
                 result = result.Remove(result.Length - 1);
             }
-            string localFilename = String.Format( @"C:\Users\{0}\Downloads\Waifu.jpg", UserName);
-            using (WebClient client = new WebClient())
+            string localFilename;
+            if (result.Substring(result.Length-1) == "f")
+            {
+                localFilename = "WaifuGif.gif";
+                try
+                {
+                    System.IO.File.Delete("WaifuJpg.jpg");
+                }
+                catch
+                {
+                    
+                }
+            }
+            else
+            {
+                localFilename = "WaifuJpg.jpg";
+                try
+                {
+                    System.IO.File.Delete("WaifuGif.gif");
+                }
+                catch
+                {
+
+                }
+            }
+            using (System.Net.WebClient client = new System.Net.WebClient())
             {
                 client.DownloadFile(result, localFilename);
             }
-            Console.WriteLine("Check your download directory");
-            Console.WriteLine("Посмотри в папку загрузки");
+            Console.WriteLine("Done, check the folder");
+            Console.ReadLine();
         }
     }
 }
